5eee83d6f02121a9b1c62a2dee1d87fc40a141ca
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-02-20 14:29:25 -0500

    nomsg

--- mainProject/src/car_understanding/experiments.py
+++ mainProject/src/car_understanding/experiments.py
@@ -230,45 +230,34 @@ def classes_for_attribs():
 
 def cv_for_params():
   from sklearn.externals.joblib import load
-  from sklearn.cross_validation import StratifiedKFold
   from sklearn.grid_search import GridSearchCV
-  from sklearn.metrics import classification_report
-  from sklearn.metrics import precision_score
-  from sklearn.metrics import recall_score
   from sklearn.svm import SVC
-  from pprint import pprint
   
   (X, y) = load('features_eq.tmp')
-  # split the dataset in two equal part respecting label proportions
-  train, test = iter(StratifiedKFold(y, 2)).next()
   
   # Set the parameters by cross-validation
   tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-4],
                        'C': [1, 10, 100, 1000]},
                       {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
   
-  scores = [
-      ('precision', precision_score),
-      ('recall', recall_score),
-  ]
-  
-  
-  for score_name, score_func in scores:
-    clf = GridSearchCV(SVC(C=1), 
-                       tuned_parameters, 
-                       score_func=score_func,
-                       n_jobs=-2)
-    clf.fit(X[train], y[train], cv=StratifiedKFold(y[train], 5))
-    y_true, y_pred = y[test], clf.predict(X[test])
+  scores = ['precision', 'recall'] 
+  for score in scores:
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
 
-    print "Classification report for the best estimator: "
-    print clf.best_estimator_
-    print "Tuned for '%s' with optimal value: %0.3f" % (
-        score_name, score_func(y_true, y_pred))
-    print classification_report(y_true, y_pred)
-    print "Grid scores:"
-    pprint(clf.grid_scores_)
-    print
+    clf = GridSearchCV(SVC(C=1), tuned_parameters, cv=5, scoring=score)
+    clf.fit(X, y)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_estimator_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    for params, mean_score, scores in clf.grid_scores_:
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean_score, scores.std() / 2, params))
+    print()
 
 
 if __name__ == '__main__':
