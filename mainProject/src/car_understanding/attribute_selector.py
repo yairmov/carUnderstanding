d14b56b4df2a2526be1588ab6a620ad91f574983
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-02-21 15:42:38 -0500

    nomsg

--- mainProject/src/car_understanding/attribute_selector.py
+++ mainProject/src/car_understanding/attribute_selector.py
@@ -36,9 +36,9 @@ class AttributeSelector:
                                index = classes.index)
     for class_index in attrib_meta.index:
       class_name = classes.class_name[class_index]
-    for name in attrib_meta.columns:
-      attrib_meta.ix[class_index, name] = \
-      AttributeSelector.has_attribute_by_name(class_name, name)
+      for name in attrib_meta.columns:
+        attrib_meta.ix[class_index, name] = \
+        AttributeSelector.has_attribute_by_name(class_name, name)
       
     return attrib_meta
     
