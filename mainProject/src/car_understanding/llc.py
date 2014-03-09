03f90295ff3cf7b9861de4f73ba42f07a0a23564
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-03-09 17:23:28 -0400

    nomsg

--- mainProject/src/car_understanding/llc.py
+++ mainProject/src/car_understanding/llc.py
@@ -40,7 +40,7 @@ def LLC_encoding(B, X, knn=5, beta=3e-2):
     print ii
     idx = IDX[ii,:]
     z = B[idx,:] - X[ii,:]     # shift ith pt to origin
-    print "B_idx", B[idx,:]
+    print "B_idx", B[idx,0:10]
     C = z.dot(z.T)             # local covariance
     C = C + II*beta*np.trace(C)   # regularlization (K>D)
     w = linalg.lstsq(C, np.ones([knn, 1]))[0]
