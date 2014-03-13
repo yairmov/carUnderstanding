6b916677bb821ba49e510c419b29218ed383222f
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-03-13 10:09:25 -0400

    nomsg

--- mainProject/src/car_understanding/small_run.py
+++ mainProject/src/car_understanding/small_run.py
@@ -263,9 +263,9 @@ def run_attrib_training(args, cross_validation=False):
 #   return
 
   # Assign cluster labels to all images
-#   print "Assigning to histograms"
-#   Bow.create_word_histograms_on_dataset(dataset['train_annos'], config)
-#   return
+  print "Assigning to histograms"
+  Bow.create_word_histograms_on_dataset(dataset['train_annos'], config)
+  return
 
   # Train attribute classifiers
   print "Training attribute classifiers"
