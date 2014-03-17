5cf700b781129e9291dd6292b320540c0008eeda
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-03-17 09:49:05 -0400

    nomsg

--- mainProject/src/car_understanding/experiments.py
+++ mainProject/src/car_understanding/experiments.py
@@ -350,8 +350,8 @@ def classify_using_attributes():
   res = bnet.create_attrib_res_on_images()
   
   # define a classifier that uses the attribute scores
-#   clf = RandomForestClassifier(n_estimators=50, n_jobs=-1)
-  clf = svm.SVC(kernel='rbf')
+  clf = RandomForestClassifier(n_estimators=50, n_jobs=-1)
+#   clf = svm.SVC(kernel='rbf')
   
   scores = cross_validation.cross_val_score(clf, res[attrib_names], 
                                             res.class_index, cv=2)
