cd34d1a1bb823c18c98bfb5acfbbf14603c24e6f
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-03-24 17:28:11 -0400

    nomsg

--- mainProject/src/car_understanding/util.py
+++ mainProject/src/car_understanding/util.py
@@ -224,8 +224,8 @@ def plot_dataset_embedding(dataset, config, title=None):
     p_bar.animate(ii)
     img_name = dataset.iloc[ii]['basename']
     img_name = os.path.splitext(img_name)[0]
-#     hist_filename = os.path.join(config.SIFT.BoW.hist_dir,
-#                                  img_name) + '_hist.dat'
+    hist_filename = os.path.join(config.SIFT.BoW.hist_dir,
+                                 img_name) + '_hist.dat'
 #     hist = Bow.load(hist_filename) 
 #     features[ii, :] = hist
      
