0c29a362ddcb416098a17687e90484b81048a452
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-03-23 22:35:50 -0400

    nomsg

--- mainProject/src/car_understanding/fgcomp_dataset_utils.py
+++ mainProject/src/car_understanding/fgcomp_dataset_utils.py
@@ -144,7 +144,7 @@ def create_dev_set(train_annos, config):
     curr = train_annos[train_annos.class_index == id]
     
     r_ids = R.permutation(curr.index)
-    print r_ids.head()
+    print r_ids
     import sys; sys.exit(0)
     dev_img_ids_test.extend(list(r_ids[:num_test]))
     dev_img_ids_train.extend(list(r_ids[num_test:]))
