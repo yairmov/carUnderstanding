b1a183911adc474b1922277ca0840936bc8b964f
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-03-24 17:51:39 -0400

    nomsg

--- mainProject/src/car_understanding/util.py
+++ mainProject/src/car_understanding/util.py
@@ -271,8 +271,8 @@ def plot_embedding(X, y=None, images=None, title=None):
 #           c=y / float(len(labels)), 
 #           marker='o', cmap=pl.cm.Set1, alpha=0.6, linewidths=1)
   for i in range(len(y)):
-    pl.text(X[i, 0], X[i, 1], str(y[i]),
-            color=pl.cm.Set1(y[i] / 10.),
+    pl.text(X[i, 0], X[i, 1], str(y.iloc[i]),
+            color=pl.cm.Set1(y.iloc[i] / 10.),
             fontdict={'weight': 'bold', 'size': 9})
 
   if (not images is None) and hasattr(offsetbox, 'AnnotationBbox'):
