4010717e3a6f0a4ee7e60fe37f656fac645f7efc
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-03-24 17:19:04 -0400

    nomsg

--- mainProject/src/car_understanding/util.py
+++ mainProject/src/car_understanding/util.py
@@ -218,6 +218,7 @@ def plot_dataset_embedding(dataset, config, title=None):
   n_items = dataset.shape[0]
   features = np.empty(shape=[n_items, config.SIFT.BoW.num_clusters])
   p_bar = ProgressBar(n_items)
+  print(p_bar)
   print('Loading {} BoW from disk'.format(n_items))
   for ii in range(n_items):
     img_name = dataset.iloc[ii]['basename']
