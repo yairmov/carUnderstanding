551673638eb00e31647407b7d4c621351b77b1b5
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-03-25 10:53:55 -0400

    nomsg

--- mainProject/src/car_understanding/util.py
+++ mainProject/src/car_understanding/util.py
@@ -294,7 +294,7 @@ def plot_embedding(X, y=None, images=None, title=None):
         continue
       shown_images = np.r_[shown_images, [X[i]]]
       imagebox = offsetbox.AnnotationBbox(
-          offsetbox.OffsetImage(images[i], cmap=pl.cm.gray_r),
+          offsetbox.OffsetImage(images[i], cmap=pl.cm.gray_r, alpha=0.6),
           X[i])
       ax.add_artist(imagebox)
       
