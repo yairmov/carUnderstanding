173bad105333d5c19ecc2b497212ca8ba4b01e23
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-01 23:26:40 -0400

    nomsg

--- mainProject/src/car_understanding/dense_sift.m
+++ mainProject/src/car_understanding/dense_sift.m
@@ -1,19 +1,25 @@
-function dense_sift(tmp_dir_name)
+% function dense_sift(tmp_dir_name)
   % add vlfeat to path
   run('/usr0/home/ymovshov/Documents/Research/Code/thirdParty/vlfeat/toolbox/vl_setup');
+  fprintf('loaded vlfeat\n')
+
 
   % Load data from python
+  fprintf('loading data\n')
   data = load(fullfile(tmp_dir_name, 'data.mat'));
   img_names = data.img_cell;
   out_names = data.data_cell;
 
   n_imgs = length(img_names)
+  fprintf('running dense sift on %d images\n', n_imgs)
+
   for i=1:n_imgs
     im = imread(img_names{i})
     [frames, descrs] = vl_phow(im2single(im), 'step', 4, 'sizes', [8 12 16 24 30]);
     save(out_names{i}, 'frame', 'descrs')
   end
+  fprintf('done!\n')
 
 
   quit;
-end
+% end
