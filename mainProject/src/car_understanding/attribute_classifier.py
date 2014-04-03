b4ff32cd921677cefa8b3c04da3f45a75c68155e
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-03 18:24:14 -0400

    nomsg

--- mainProject/src/car_understanding/attribute_classifier.py
+++ mainProject/src/car_understanding/attribute_classifier.py
@@ -228,7 +228,7 @@ class AttributeClassifier:
     
   @staticmethod  
   def save(attrib_classifier, filename):
-    dump(attrib_classifier, filename, compress=3)
+    dump(attrib_classifier, filename, compress=0)
       
   @staticmethod    
   def load(filename):
