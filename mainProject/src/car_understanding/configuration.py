13efd22f6997d062e2c2f6a3c3b75b7e2e827679
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-03 19:12:02 -0400

    nomsg

--- mainProject/src/car_understanding/configuration.py
+++ mainProject/src/car_understanding/configuration.py
@@ -38,7 +38,7 @@ def get_config(args):
   # Number of clusters after clustering has been done (<= requested_n_clusters)
   config.SIFT.BoW.num_clusters = 4000 #(TODO: find a nice way to do this)
   config.SIFT.BoW.max_desc_per_img = 1000
-  config.SIFT.BoW.max_desc_total = 6e6
+  config.SIFT.BoW.max_desc_total = 4e6
 
   # SIFT-LLC
   config.SIFT.LLC.use = True
