5ab738a12f527b771a04e4395f6f24fe76c2a443
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-05 19:51:15 -0400

    nomsg

--- mainProject/src/car_understanding/visualization.py
+++ mainProject/src/car_understanding/visualization.py
@@ -47,7 +47,8 @@ def showboxes(img_in, boxes, is_opencv=False):
 
 def explore_image_data(annos, config):
   plt.ion()
-  for ii in range(0, len(annos)):
+  n_imgs = len(annos)
+  for ii in reversed(range(n_imgs)):
     curr_anno = annos.iloc[ii]
     img = scipy.misc.imread(os.path.join(
                       config.dataset.main_path, curr_anno['rel_path']))
