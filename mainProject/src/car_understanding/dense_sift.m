b19cf8f38be5be04fd4139b0502bf9554c4e844c
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-05 23:53:25 -0400

    nomsg

--- mainProject/src/car_understanding/dense_sift.m
+++ mainProject/src/car_understanding/dense_sift.m
@@ -25,8 +25,8 @@ function dense_sift(tmp_dir_name)
   for i=1:1
     fprintf('%s\n', img_names{i})
     im = imread(img_names{i});
-    return
     [frames, desc] = vl_phow(im2single(im), 'step', 4, 'sizes', sizes, 'FloatDescriptors', true);
+    return
     frames = frames'; % now each row of frames is: [x, y, ???, patch_size]
     desc = desc';
     save(out_names{i}, 'frames', 'desc', '-v7');
