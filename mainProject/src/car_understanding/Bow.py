9f128ee1757c083542f498563c7ad1174449f3d1
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-06 19:41:14 -0400

    nomsg

--- mainProject/src/car_understanding/Bow.py
+++ mainProject/src/car_understanding/Bow.py
@@ -212,24 +212,24 @@ def create_word_histograms_on_dataset(data_annos, config):
   dir_path = config.SIFT.raw_dir
   n_files = data_annos.shape[0]
 
-  if not os.path.isdir(config.SIFT.BoW.hist_dir):
-    os.makedirs(config.SIFT.BoW.hist_dir)
+#   if not os.path.isdir(config.SIFT.BoW.hist_dir):
+#     os.makedirs(config.SIFT.BoW.hist_dir)
 
-  Parallel(n_jobs=config.n_cores, verbose=config.logging.verbose)(
-                 delayed(create_word_histogram_on_file)(
-                 os.path.join(dir_path,
-                              os.path.splitext(data_annos.iloc[ii]['basename'])[0] + '.dat'),
-                 bow_model,
-                 config)
-                 for ii in range(n_files))
+#   Parallel(n_jobs=config.n_cores, verbose=config.logging.verbose)(
+#                  delayed(create_word_histogram_on_file)(
+#                  os.path.join(dir_path,
+#                               os.path.splitext(data_annos.iloc[ii]['basename'])[0] + '.dat'),
+#                  bow_model,
+#                  config)
+#                  for ii in range(n_files))
 
-#   for ii in range(n_files):
-# #   for ii in [13714]:
-#     print ii
-#     create_word_histogram_on_file(os.path.join(dir_path,
-#                                 os.path.splitext(data_annos.iloc[ii]['basename'])[0] + '.dat'),
-#                                 bow_model,
-#                                 config)
+  for ii in range(n_files):
+#   for ii in [13714]:
+    print ii
+    create_word_histogram_on_file(os.path.join(dir_path,
+                                os.path.splitext(data_annos.iloc[ii]['basename'])[0] + '.dat'),
+                                bow_model,
+                                config)
 
 
 def load_bow(data_annos, config):
