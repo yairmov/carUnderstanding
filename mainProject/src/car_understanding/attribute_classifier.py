985f2e195774c0b85066cbc58d5c67044d6a118e
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-09 14:21:24 -0400

    nomsg

--- mainProject/src/car_understanding/attribute_classifier.py
+++ mainProject/src/car_understanding/attribute_classifier.py
@@ -52,8 +52,9 @@ class AttributeClassifier:
 #                            probability=True)
 
 #     self.clf          = LinearSVC(class_weight='auto', loss='l2', C=1e-3)
-    self.clf          = RandomForestClassifier(n_estimators=200, max_depth=5,
-                                               min_samples_split=4,
+    self.clf          = RandomForestClassifier(n_estimators=100, max_depth=5,
+                                               min_samples_split=1,
+                                               min_samples_leaf=1,
                                                oob_score=True,
                                                n_jobs=self.n_cores)
 #     self.clf          = GradientBoostingClassifier(n_estimators=1000, 
