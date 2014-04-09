ea2e86382e9f54a8c3b2626dbc9f92cfe16adc4b
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-09 17:55:57 -0400

    nomsg

--- mainProject/src/car_understanding/experiments.py
+++ mainProject/src/car_understanding/experiments.py
@@ -430,8 +430,8 @@ def classify_using_sift():
   assert np.array_equal(np.unique(labels_train), 
                         np.unique(labels_test)), 'test labels not equal train labels'
   
-  clf = RandomForestClassifier(n_estimators=10000, 
-                               max_depth=10,
+  clf = RandomForestClassifier(n_estimators=1000, 
+                               max_depth=50,
                                min_samples_split=1,
                                min_samples_leaf=1,
                                oob_score=True,
