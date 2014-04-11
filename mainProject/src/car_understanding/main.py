be0bdd1dfeb645cab93a100d02f54943e5fbc1b6
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-11 13:49:12 -0400

    nomsg

--- mainProject/src/car_understanding/main.py
+++ mainProject/src/car_understanding/main.py
@@ -66,13 +66,13 @@ def load_sift(data_annos, config):
   fnames = data_annos.basename.apply(lambda x: path(x).splitext()[0] + '.dat')
   fnames = list(fnames.apply(lambda x: path(config.SIFT.raw_dir).joinpath(x)))
   print 'Loading dense SIFT from %d images ' % nfiles
-#   features = Parallel(n_jobs=-1, verbose=config.logging.verbose)(
-#                  delayed(load_sift_from_file)(fnames[ii], config.SIFT.BoW.max_desc_per_img)
-#                  for ii in range(nfiles))
+  features = Parallel(n_jobs=-1, verbose=config.logging.verbose)(
+                 delayed(load_sift_from_file)(fnames[ii], config.SIFT.BoW.max_desc_per_img)
+                 for ii in range(nfiles))
 
-  features = []
-  for ii in range(nfiles):
-    features.append(load_sift_from_file(fnames[ii], config.SIFT.BoW.max_desc_per_img))
+#   features = []
+#   for ii in range(nfiles):
+#     features.append(load_sift_from_file(fnames[ii], config.SIFT.BoW.max_desc_per_img))
   
   features = np.concatenate(features)
   # sample max_desc features
