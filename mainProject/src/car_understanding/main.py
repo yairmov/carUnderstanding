a58184cf28d828d6496f2cd021c4e21e9b845f2a
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-10 23:55:34 -0400

    nomsg

--- mainProject/src/car_understanding/main.py
+++ mainProject/src/car_understanding/main.py
@@ -4,10 +4,16 @@ Created on Mar 3, 2014
 @author: ymovshov
 '''
 
+import numpy as np
+from path import path
+from sklearn.externals.joblib import Parallel, delayed
+
 from configuration import get_config, update_config
 import util
 from matlab_dense_sift import dense_sift_matlab
 import fgcomp_dataset_utils as fgu
+from dense_SIFT import load_from_disk
+import Bow
 
 
 def preprocess_dataset(config):
@@ -36,6 +42,41 @@ def calculate_dense_sift(data_annos, config):
   dense_sift_matlab(data_annos, config)
   
 
+
+def load_sift_from_file(sift_filename, max_num_desc=None):
+  (kp, desc) = load_from_disk(sift_filename, matlab_version=True)
+  
+  
+  # Randomly select a scale and get all the descriptors from it
+  p_size = np.random.choice(np.unique(kp[:,-1]))
+  inds = kp[:,-1] == p_size
+  kp = kp[inds, :]
+  desc = desc[inds, :]
+  
+#   # Random selection of a subset of the descriptors
+#   inds  = np.random.permutation(desc.shape[0])
+#   desc = desc[inds, :]
+#   desc = desc[:max_num_desc, :]
+  
+  return desc
+  
+  
+def load_sift(data_annos, config):
+  nfiles = len(data_annos)
+  fnames = data_annos.basename.apply(lambda x: path(x).splitext()[0] + '.dat')
+  fnames = list(fnames)
+  print 'Loading dense SIFT from %d images ' % nfiles
+  features = Parallel(n_jobs=-1, verbose=config.logging.verbose)(
+                 delayed(load_sift_from_file)(data_annos[ii], config)
+                 for ii in range(nfiles))
+  
+  features = np.concatenate(features)
+  # sample max_desc features
+  inds  = np.random.permutation(features.shape[0])
+  features = features[inds, :]
+  features = features[:config.SIFT.BoW.max_desc_total, :]
+
+  return features
   
 def main():
   config = get_config()
@@ -49,10 +90,10 @@ def main():
   calculate_dense_sift(dataset['test_annos'], config)
   
   # Create BoW model
-#   features = load_SIFT_from_files(dataset, config)
-#   print "Loaded %d SIFT features from disk" % features.shape[0]
-#   print "K-Means CLustering"
-#   bow_model = Bow.create_BoW_model(features, config)
+  features = load_sift(dataset, config)
+  print "Loaded %d SIFT features from disk" % features.shape[0]
+  print "K-Means CLustering"
+  bow_model = Bow.cluster_to_words(features, config)
   
   
     
