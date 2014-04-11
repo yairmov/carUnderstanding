6bb893bfe0b866a67ca4a84a4cc05d1a893863e1
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-10 21:01:52 -0400

    nomsg

--- mainProject/src/car_understanding/main.py
+++ mainProject/src/car_understanding/main.py
@@ -6,11 +6,11 @@ Created on Mar 3, 2014
 
 from configuration import get_config, update_config
 import util
+from matlab_dense_sift import dense_sift_matlab
+import fgcomp_dataset_utils as fgu
 
-if __name__ == '__main__':
-  # Load config
-  config = get_config()
-  
+
+def preprocess_dataset(config):
   # Copy data set to cache
   util.copy_dataset('../../../fgcomp2013/release', 
                     config)
@@ -28,6 +28,36 @@ if __name__ == '__main__':
                                  config.dataset.main_path,
                                  config.bb_area)
     update_config(config, 'dataset.is_cropped', True)
+
+
+
+
+def calculate_dense_sift(data_annos, config):  
+  dense_sift_matlab(data_annos, config)
+  
+
+  
+def main():
+  config = get_config()
+  
+  preprocess_dataset(config)
+  
+  (dataset, config) = fgu.get_all_metadata(config)
+  
+  calculate_dense_sift(dataset['train_annos'], config)
+  calculate_dense_sift(dataset['test_annos'], config)
+  
+  # Create BoW model
+#   features = load_SIFT_from_files(dataset, config)
+#   print "Loaded %d SIFT features from disk" % features.shape[0]
+#   print "K-Means CLustering"
+#   bow_model = Bow.create_BoW_model(features, config)
+  
+  
+    
+if __name__ == '__main__':
+  main()
+  
     
     
   
