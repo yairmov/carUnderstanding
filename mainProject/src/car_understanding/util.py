b7256873db793ab98cc4939a48273fef81ef2e0f
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-12 11:58:16 -0400

    nomsg

--- mainProject/src/car_understanding/util.py
+++ mainProject/src/car_understanding/util.py
@@ -164,7 +164,7 @@ def crop_and_resize_dataset(infile, outfile, main_path, bb_area):
 def copy_dataset(config):
   if config.dataset.main_path.isdir() and \
      len(config.dataset.main_path.listdir()) > 0:
-    print('FOUND COPY OF DATASET, NOT DOING ANYTHING')
+    print('FOUND COPY OF DATASET, NOT COPYING ANYTHING')
     return
   
   old_path = config.dataset.original_dataset_path
