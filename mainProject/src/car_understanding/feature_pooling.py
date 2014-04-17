655e5bf6d38b1510854653e28f3277d4c52cfd61
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-17 17:27:21 -0400

    nomsg

--- mainProject/src/car_understanding/feature_pooling.py
+++ mainProject/src/car_understanding/feature_pooling.py
@@ -44,7 +44,7 @@ class SpatialPooler(object):
     
   
   @staticmethod
-  @jit(nopython=True)
+  @jit('f8[:](f8[:], f8[:], f8[:])', nopython=True)
   def to_pool(locations, features, pooling_box):
     M = locations.max(axis = 0)
     
@@ -72,7 +72,7 @@ class SpatialPooler(object):
                                            'where each row is the x,y '+
                                            'location in the image')
     
-    return self.to_pool(locations, features, self.pooling_box)
+    return self.to_pool(locations, features, np.array(self.pooling_box))
     
     
     
