36f8e181a744674137740c7c74358f236816ff6b
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-17 16:34:25 -0400

    nomsg

--- mainProject/src/car_understanding/feature_pooling.py
+++ mainProject/src/car_understanding/feature_pooling.py
@@ -7,7 +7,7 @@ Created on Apr 17, 2014
 import numpy as np
 from numba import autojit, jit
 
-@jit('b1[:](f8[:], f8[:])')
+# @jit('b1[:](f8[:], f8[:])')
 def contains(box, points):
   '''
   For each point in points checks if it is in the box.
