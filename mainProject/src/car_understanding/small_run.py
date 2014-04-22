fa6236eb5f099f5b3a658de0c12cb94b7f9fd09c
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-22 19:30:52 -0400

    nomsg

--- mainProject/src/car_understanding/small_run.py
+++ mainProject/src/car_understanding/small_run.py
@@ -484,7 +484,7 @@ def bayes_net_generic(use_gt=False):
   (class_probs, attrib_probs) = bnet.predict(test_annos.iloc[:2])
   dump({'class_probs': class_probs, 'attrib_probs': attrib_probs},
        'bnet_res.dat')
-  show_confusion_matrix(test_annos, classes, class_probs)
+#   show_confusion_matrix(test_annos, classes, class_probs)
   
   
   
