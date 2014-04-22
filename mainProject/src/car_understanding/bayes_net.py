3f8bf613be4950fdcc1826ec18e1dc0be2b51855
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-22 14:30:24 -0400

    nomsg

--- mainProject/src/car_understanding/bayes_net.py
+++ mainProject/src/car_understanding/bayes_net.py
@@ -108,6 +108,10 @@ class BayesNet:
   
     res = pd.concat([res, train_annos.ix[:, ['class_index']]], axis=1)
     res_descrete = pd.concat([res_descrete, train_annos.ix[:, ['class_index']]], axis=1)
+    
+    sklearn.externals.joblib.dump({'res': res, 'res_descrete': res_descrete}, 'tmp.dat')
+    import sys; sys.exit(0)
+    
     return res, res_descrete
   
   
