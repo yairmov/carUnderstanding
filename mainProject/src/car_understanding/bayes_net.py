2f72f93b6d6dfbc36dab115cdf7c21a24dd3dc0d
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-23 10:25:19 -0400

    nomsg

--- mainProject/src/car_understanding/bayes_net.py
+++ mainProject/src/car_understanding/bayes_net.py
@@ -235,7 +235,6 @@ class BayesNet:
         print 'Attribute: {}'.format(attrib_name)
         self.CPT['p({}|theta)'.format(attrib_name)] = \
           self.cpt_for_attrib(attrib_name, attrib_selector)
-        import code; code.interact(local=locals())
 #         pbar.animate(ii)
 #       print ''
         
