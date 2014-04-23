63166b76d061bcf512182190269aaa318f7d5039
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-22 21:17:17 -0400

    nomsg

--- mainProject/src/car_understanding/train_and_test_attribute_classifiers.py
+++ mainProject/src/car_understanding/train_and_test_attribute_classifiers.py
@@ -95,8 +95,6 @@ def test(args, config, dataset):
   print("========")
   print("")
   test_annos = dataset['test_annos']
-  print test_annos.shape
-  return
   attrib_selector = AttributeSelector(config, dataset['class_meta'])
   
   print "Load image Bow histograms from disk"
