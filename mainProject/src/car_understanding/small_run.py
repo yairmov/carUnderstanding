089e062a3691950a3f30f7001b596fa6bdda3fb1
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-28 14:00:49 -0400

    nomsg

--- mainProject/src/car_understanding/small_run.py
+++ mainProject/src/car_understanding/small_run.py
@@ -473,9 +473,9 @@ def bayes_net_generic(use_gt=False):
   
   test_annos = dataset['test_annos']
   # Select only images from the args "world"
-#   test_annos = test_annos[np.array(
-#                              test_annos.class_index.isin(classes.class_index))]
-  
+# #   test_annos = test_annos[np.array(
+# #                              test_annos.class_index.isin(classes.class_index))]
+#   
 #   print 'TESTING ON TRAINING!!!!!!!'
 #   test_annos = train_annos.iloc[:train_annos.shape[0]/2]
   
