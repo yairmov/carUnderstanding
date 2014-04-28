147742ae7c5e84797bf16574f6337cada4fdeeff
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-28 17:11:08 -0400

    nomsg

--- mainProject/src/car_understanding/bayes_net.py
+++ mainProject/src/car_understanding/bayes_net.py
@@ -415,8 +415,8 @@ class BayesNet:
 #       MAP.fit() # first do MAP estimation
       
     mcmc = mc.MCMC(model)
-    mcmc.sample(20000, 3000)
-#     mcmc.sample(2000)
+#     mcmc.sample(10000, 3000)
+    mcmc.sample(2000)
     print()
 
 ##     use    
