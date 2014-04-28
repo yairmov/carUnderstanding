64e46860273cb7bb3451397f8049c57c11923f4f
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-28 11:21:00 -0400

    nomsg

--- mainProject/src/car_understanding/visualization.py
+++ mainProject/src/car_understanding/visualization.py
@@ -123,7 +123,7 @@ def create_image_page(img_files, html_file, width=200, num_per_row=9,
 
     if (k == num_per_row):
       if not(usr_str is None):
-        html_str += '<h1>' + usr_str[group] + '</h1><br> <hr>'
+        html_str += '<h2>' + usr_str[group] + '</h2><br> <hr>'
 #         html_str += '<hr>' + '<center> <h2> ' + usr_str[group] + '</h2></center> <br> <hr>'
         group += 1
       k = 0
