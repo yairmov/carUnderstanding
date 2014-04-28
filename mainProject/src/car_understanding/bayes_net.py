9aa7039ed9a674d5210d0e776c7640532ce51f2c
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-28 14:30:03 -0400

    nomsg

--- mainProject/src/car_understanding/bayes_net.py
+++ mainProject/src/car_understanding/bayes_net.py
@@ -412,13 +412,13 @@ class BayesNet:
     model = mc.Model(nodes)
 #     mc.graph.dag(model).write_pdf('tmp.pdf')
     
-    if not self.use_gt:
-      MAP = mc.MAP(model)
-      MAP.fit() # first do MAP estimation
+#     if not self.use_gt:
+#       MAP = mc.MAP(model)
+#       MAP.fit() # first do MAP estimation
       
     mcmc = mc.MCMC(model)
-#     mcmc.sample(10000, 3000)
-    mcmc.sample(2000)
+    mcmc.sample(10000, 3000)
+#     mcmc.sample(2000)
     print()
 
 ##     use    
