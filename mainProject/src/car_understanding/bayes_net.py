127de21190bc069cccee9afd6c7e40ac5c1e4867
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-28 17:24:23 -0400

    nomsg

--- mainProject/src/car_understanding/bayes_net.py
+++ mainProject/src/car_understanding/bayes_net.py
@@ -408,7 +408,7 @@ class BayesNet:
     nodes.extend(class_bnet_nodes.values())
     nodes.extend(theta)
     model = mc.Model(nodes)
-#     mc.graph.dag(model).write_pdf('tmp.pdf')
+    mc.graph.dag(model).write_pdf('tmp.pdf')
     
 #     if not self.use_gt:
 #       MAP = mc.MAP(model)
