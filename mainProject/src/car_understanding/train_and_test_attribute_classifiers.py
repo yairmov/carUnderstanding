2d7cb4c39da06743b87babd99be159a0df7345ec
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-04-28 09:05:17 -0400

    nomsg

--- mainProject/src/car_understanding/train_and_test_attribute_classifiers.py
+++ mainProject/src/car_understanding/train_and_test_attribute_classifiers.py
@@ -149,7 +149,7 @@ def test(args, config, dataset):
     print("--------------{}-------------".format(attrib_name)) 
     
     print(classification_report(true_labels, 
-                                np.array(res[str.lower(attrib_name)]) > 0.5, 
+                                np.array(res[str.lower(attrib_name)]) > 0, 
                                 target_names=['not-{}'.format(attrib_name),
                                               attrib_name]))
     
