8ee552251dc8f6cc4d12ae8b1ed8f2aadf5729e5
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-05-01 14:28:01 -0400

    nomsg

--- mainProject/src/car_understanding/bayes_net.py
+++ mainProject/src/car_understanding/bayes_net.py
@@ -88,8 +88,10 @@ class BayesNet2():
     class_priors = class_counts / n_imgs
     
     for ii in range(len(self.class_inds)):
-      prior = pd.DataFrame(data=[class_priors[ii], 1-class_priors[ii]],
-                           columns=['True', 'False'])
+      prios = pd.DataFrame({'True': class_priors[ii],
+                            'False': 1-class_priors[ii]})
+#       prior = pd.DataFrame(data=[class_priors[ii], 1-class_priors[ii]],
+#                            columns=['True', 'False'])
       self.CPT['p({})'.format(self.class_inds[ii])] = prior
     
     print self.CPT    
