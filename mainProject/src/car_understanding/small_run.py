8418503f4c80599e6dbc9de4e087e835d8850eeb
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-05-05 17:44:12 -0400

    nosmg

--- mainProject/src/car_understanding/small_run.py
+++ mainProject/src/car_understanding/small_run.py
@@ -488,9 +488,8 @@ def bayes_net_generic(use_gt=False):
   bnet = bayes_net.BayesNet2(config, train_annos, 
                   classes, attrib_classifiers, attrib_meta, 
                   multi_class_clf=m_clf, desc=str(args), use_gt=use_gt)
-
-  bnet.init()  
   dump({'bnet': bnet}, '../../../output/bnet.dat')
+  bnet.init()  
   return
     
   
