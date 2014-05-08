18e0189e84d8b8dc3f7808c08c9e423fc2c4a290
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-05-08 11:20:07 -0400

    nomsg

--- mainProject/src/car_understanding/bayes_net.py
+++ mainProject/src/car_understanding/bayes_net.py
@@ -182,9 +182,9 @@ class BayesNet2():
       c[1,:], b = np.histogram(n_scores, bins)
       normalize(c, axis=1, copy=False)
       
-      print '[{}, {}]'.format(p_scores.min(), p_scores.max())
-      print c
-      import sys;sys.exit(0)
+#       print '[{}, {}]'.format(p_scores.min(), p_scores.max())
+#       print c
+#       import sys;sys.exit(0)
       
       cpt = pd.DataFrame(data=c,
                          index=['True', 'False'], 
