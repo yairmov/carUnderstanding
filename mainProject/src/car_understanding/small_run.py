1c9d4522b21dd9de61fe79f7ec2756305b699b62
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-05-08 11:34:18 -0400

    nomsg

--- mainProject/src/car_understanding/small_run.py
+++ mainProject/src/car_understanding/small_run.py
@@ -488,7 +488,8 @@ def bayes_net_generic(use_gt=False):
                   multi_class_clf=m_clf, desc=str(args), use_gt=use_gt)
   dump({'bnet': bnet}, '../../../output/bnet.dat')
   bnet.init()   
-  
+  return
+
   print 'predicting!!!'
 
   (class_probs, attrib_probs) = bnet.predict(test_annos)
