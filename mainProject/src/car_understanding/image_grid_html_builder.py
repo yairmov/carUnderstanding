ba66ff64dc36f803f0bd69796dd2360a212900bf
Yair Movshovitz-Attias <yair@cs.cmu.edu>
2014-09-20 00:54:15 -0700

    Added sorting functionality to image grid class

--- mainProject/src/car_understanding/image_grid_html_builder.py
+++ mainProject/src/car_understanding/image_grid_html_builder.py
@@ -20,22 +20,60 @@ class ImageGridHtmlBuilder(object):
     super(ImageGridHtmlBuilder, self).__init__()
     self.html_data_ = ''
     self.box_id_ = 0
+    self.with_sort_data_ = False
+    self.sort_def_ = None
+    self.sort_by_dict_ = None
 
 
   # def AddRuler(self, title=''):
   #   self.html_data_ += '<div class="stamp stamp1"></div>'
     # self.html_data_ += '<hr>' + '<center> <h2> ' + str(title) + '</h2></center> <br> <hr>'
 
-  def AddBox(self, img, title):
+  def AddBox(self, img, title, meta_dict={'confidence': 0}):
       img_base_64 = ImageToBase64(img)
       img_txt = kImageTemplate.format(data=img_base_64)
-      box_str = kTableTemplate.format(data=img_txt, title=title, bid=str(self.box_id_))
+
+      meta_str = ''
+      for key, value in meta_dict.items():
+        meta_str += ' {0}="{1}"'.format(key, value)
+      meta_str += ' bid="{}"'.format(self.box_id_)
+      box_str = kTableTemplate.format(data=img_txt, title=title,
+                                                        meta=meta_str)
       self.html_data_ +=  box_str + '\n'
       self.box_id_ += 1
 
+  def AddSortingFunctionality(self, sort_by_dict):
+    self.sort_by_dict_ = sort_by_dict
+    get_sort_data = ''
+    for key, is_number in sort_by_dict.items():
+      number_keyword = ''
+      if is_number:
+        number_keyword = 'parseFloat'
+      get_sort_data += '{key}: "[{key}] {keyword}", \n'.format(key=key, keyword=number_keyword)
+
+    self.sort_def_ = get_sort_data
+    self.with_sort_data_ = True
+
+
   def SaveToFile(self, filename, title='Untitled'):
-    data = kHtmlTemplate.replace('__BODY__', self.html_data_)
-    data = data.replace('__TITLE__', title)
+    data = kHtmlTemplate.replace('__TITLE__', title)
+
+    if self.with_sort_data_:
+      data = data.replace('__GET_SORT_DATA__', self.sort_def_)
+      # Add buttons
+      button_str = '<div id="sorts" class="button-group">\n'
+      button_str +='<button class="button is-checked " data-sort-by="original-order">original order</button>\n'
+      for key in self.sort_by_dict_.keys():
+        button_str +='<button class="button " data-sort-by="{0}">{0}</button>\n'.format(key)
+      button_str += '</div>\n'
+
+    else:
+      # data = data.replace('__SORT_BY__', '')
+      data = data.replace('__GET_SORT_DATA__', '')
+
+    data = data.replace('__BODY__', self.html_data_ )
+    data = data.replace('__BUTTONS__', button_str)
+
     with open(filename, "wb") as out_file:
       out_file.write(data)
 
@@ -93,6 +131,61 @@ kHtmlTemplate = '''<!doctype html>
            -o-transition-property: left, right, top;
               transition-property: left, right, top;
     }
+
+    /* ---- button ---- */
+
+.button {
+  display: inline-block;
+  padding: 0.5em 1.0em;
+  background: #EEE;
+  border: none;
+  border-radius: 7px;
+  background-image: linear-gradient( to bottom, hsla(0, 0%, 0%, 0), hsla(0, 0%, 0%, 0.2) );
+  color: #222;
+  font-family: sans-serif;
+  font-size: 16px;
+  text-shadow: 0 1px white;
+  cursor: pointer;
+}
+
+.button:hover {
+  background-color: #8CF;
+  text-shadow: 0 1px hsla(0, 0%, 100%, 0.5);
+  color: #222;
+}
+
+.button:active,
+.button.is-checked {
+  background-color: #28F;
+}
+
+.button.is-checked {
+  color: white;
+  text-shadow: 0 -1px hsla(0, 0%, 0%, 0.8);
+}
+
+.button:active {
+  box-shadow: inset 0 1px 10px hsla(0, 0%, 0%, 0.8);
+}
+
+/* ---- button-group ---- */
+
+.button-group:after {
+  content: '';
+  display: block;
+  clear: both;
+}
+
+.button-group .button {
+  float: left;
+  border-radius: 0;
+  margin-left: 0;
+  margin-right: 1px;
+}
+
+.button-group .button:first-child { border-radius: 0.5em 0 0 0.5em; }
+.button-group .button:last-child { border-radius: 0 0.5em 0.5em 0; }
+
   </style>
 
 
@@ -100,6 +193,7 @@ kHtmlTemplate = '''<!doctype html>
 
 </head>
 <body >
+__BUTTONS__
 <div id="container" class="clearfix">
 
 __BODY__
@@ -119,12 +213,44 @@ __BODY__
       isAnimated: !Modernizr.csstransitions
     });
   });
+
+  // Allow sorting
+  var $container = $('#container').isotope({
+    getSortData: {
+      __GET_SORT_DATA__
+    }
+  });
+  $container.isotope({
+    sortBy : 'original-order',
+    sortAscending: true,
+  });
+
+  // sort items on button click
+  $('#sorts').on( 'click', 'button', function() {
+    var sortByValue = $(this).attr('data-sort-by');
+    if (sortByValue == 'original-order') {
+     var sortAscendingValue = true
+    }
+    else {
+     var sortAscendingValue = false
+    }
+    $container.isotope({ sortBy: sortByValue, sortAscending: sortAscendingValue});
+  });
+  // change is-checked class on buttons
+    $('.button-group').each( function( i, buttonGroup ) {
+      var $buttonGroup = $( buttonGroup );
+      $buttonGroup.on( 'click', 'button', function() {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $( this ).addClass('is-checked');
+      });
+    });
+
 </script>
 </body>
 </html>
 '''
 
-kTableTemplate = '''<div class="box  {bid}">
+kTableTemplate = '''<div class="box"  {meta}>
       <table style="display:inline-table;">
       <tr><td>
       {data}
@@ -140,12 +266,12 @@ kImageTemplate = '''<img src="data:image/jpeg;base64,
 if __name__ == '__main__':
   img = '/Users/yair/Downloads/test.jpg'
   builder = ImageGridHtmlBuilder()
-  builder.AddBox(img, 'test image 1')
-  builder.AddBox(img, 'test image 2')
+  builder.AddBox(img, 'test image 1', meta_dict={'confidence': 1, 'class_name': 2})
+  builder.AddBox(img, 'test image 2', meta_dict={'confidence': 2, 'class_name': 1})
   # builder.AddRuler('LALA')
-  builder.AddBox(img, 'test image 3')
-  builder.AddBox(img, 'test image 4')
-  builder.AddBox(img, 'test image 4')
-  builder.AddBox(img, 'test image 4')
-  builder.AddBox(img, 'test image 4')
+  builder.AddBox(img, 'test image 3', meta_dict={'confidence': 3, 'class_name': 3})
+  builder.AddBox(img, 'test image 4', meta_dict={'confidence': 4, 'class_name': 2})
+
+  builder.AddSortingFunctionality({'confidence' : True, 'class_name': True})
+
   builder.SaveToFile('/Users/yair/Downloads/test.html', 'my test html')
